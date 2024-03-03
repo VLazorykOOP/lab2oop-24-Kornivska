25c3b57dee92d9c6f889204d83192071b7306c27
Аліна Корнівська <alinkornivska@gmail.com>
2024-03-03 16:21:14 +0200

    ввожу основну функцію main(), яка виконує основну логіку програми

--- OOPLab2Tpl/Tasks.h
+++ OOPLab2Tpl/Tasks.h
@@ -4,5 +4,22 @@ using namespace std;
 void MenuTask();
 void task1();
 void task2();
+int main() {
+    int task;
+    MenuTask(); // Виклик функції MenuTask()
+    cout << "Enter task number (1,2): ";
+    cin >> task;
+    switch (task) {
+    case 1:
+        task1();
+        break;
+    case 2:
+        task2();
+        break;
+    default:
+        cout << "Error\n";
+    }
+    return 0;
+}
 
 
